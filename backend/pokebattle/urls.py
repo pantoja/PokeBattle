4480796b77c5cbc5bd4823275707c273651c83e4
Victoria Pantoja <vicpantojadoamaral@gmail.com>
2020-02-21 12:18:34 -0300

    Refactor url file

--- backend/pokebattle/urls.py
+++ backend/pokebattle/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url  # noqa
 from django.contrib import admin
+from django.urls import path
 from django.views.generic import TemplateView
 
 import django_js_reverse.views
@@ -8,8 +9,8 @@ from battles.views import CreateBattleView
 
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
-    url(r"^jsreverse/$", django_js_reverse.views.urls_js, name="js_reverse"),
-    url(r"^$", TemplateView.as_view(template_name="itworks.html"), name="home"),
-    url(r"^create/$", CreateBattleView.as_view(), name="create_battle"),
+    path("admin/", admin.site.urls),
+    path("jsreverse/", django_js_reverse.views.urls_js, name="js_reverse"),
+    path("", TemplateView.as_view(template_name="itworks.html"), name="home"),
+    path("create/", CreateBattleView.as_view(), name="create_battle"),
 ]
