d6f0e32c6894e94a3c7bf216fb55f57cccf9458e
Victoria Pantoja <vicpantojadoamaral@gmail.com>
2020-03-16 17:29:55 -0300

    Add view

--- backend/battles/urls.py
+++ backend/battles/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from battles.views import CreateBattleView, CreateTeamView
+from battles.views import CreateBattleView, CreateTeamView, ListSettledBattlesView
 
 
 app_name = "battles"
@@ -8,4 +8,5 @@ app_name = "battles"
 urlpatterns = [
     path("create-battle/", CreateBattleView.as_view(), name="create_battle"),
     path("create-team/<int:pk>", CreateTeamView.as_view(), name="create_team"),
+    path("settled-battles/", ListSettledBattlesView.as_view(), name="settled_battles"),
 ]
