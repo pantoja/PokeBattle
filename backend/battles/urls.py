f78cf0694dd849b7e4bc893a6211a195c2c1a98a
Victoria Pantoja <vicpantojadoamaral@gmail.com>
2020-03-18 15:11:15 -0300

    Create active battles listing

--- backend/battles/urls.py
+++ backend/battles/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from battles.views import CreateBattleView, CreateTeamView, ListSettledBattlesView
+from battles.views import (
+    CreateBattleView,
+    CreateTeamView,
+    ListActiveBattlesView,
+    ListSettledBattlesView,
+)
 
 
 app_name = "battles"
@@ -9,4 +14,5 @@ urlpatterns = [
     path("create-battle/", CreateBattleView.as_view(), name="create_battle"),
     path("create-team/<int:pk>", CreateTeamView.as_view(), name="create_team"),
     path("settled-battles/", ListSettledBattlesView.as_view(), name="settled_battles"),
+    path("active-battles/", ListActiveBattlesView.as_view(), name="active-battles"),
 ]
