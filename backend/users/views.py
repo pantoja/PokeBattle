9fcea2bf8cc6791897a57791ac385b859adb4ef4
Victoria Pantoja <vicpantojadoamaral@gmail.com>
2020-03-23 16:22:52 -0300

    succesfully create user

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -10,3 +12,17 @@ class UserSignUpView(CreateView):
     form_class = UserSignUpForm
     template_name = "users/sign-up.html"
     success_url = reverse_lazy("home")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context["user_already_logged"] = True
+        return context
+
+    def form_valid(self, form):
+        super().form_valid(form)
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password1")
+        user = authenticate(email=email, password=password)
+        login(self.request, user)
+        return HttpResponseRedirect(self.get_success_url())
