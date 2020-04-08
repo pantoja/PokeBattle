439faf994fe3a8c352d7f702895280657251e378
Victoria Pantoja <vicpantojadoamaral@gmail.com>
2020-04-08 20:29:32 -0300

    Add beat to celery command in procfile

--- backend/pokebattle/celerybeat_schedule.py
+++ backend/pokebattle/celerybeat_schedule.py
@@ -5,7 +5,7 @@ CELERYBEAT_SCHEDULE = {
     # Internal tasks
     "clearsessions": {"schedule": crontab(hour=3, minute=0), "task": "users.tasks.clearsessions"},
     "save_pokemon_from_pokeapi_weekly": {
-        "schedule": crontab(hour=3, minute=00, day_of_week=4),
+        "schedule": crontab(hour=0, minute=0, day_of_week=4),
         "task": "pokemon.tasks.save_pokemon_from_pokeapi_weekly",
     },
 }
